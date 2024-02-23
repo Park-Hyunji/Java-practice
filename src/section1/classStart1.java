27f0e58b87d2705cfb5b39a0fdd38ab639448482
hyunji <phj9573@naver.com>
2024-02-24 00:29:15 +0900

    section1

---
+++ src/section1/classStart1.java
@@ -0,0 +1,17 @@
+package section1;
+
+public class classStart1 {
+    public static void main(String[] args) {
+        String studentNumber1 = "학생1";
+        int studentAge1 = 15;
+        int studentScore1 = 90;
+
+        String studentNumber2 = "학생2";
+        int studentAge2 = 16;
+        int studentScore2 = 80;
+
+        System.out.println("이름: " + studentNumber1 + " 나이: " + studentAge1 + " 성적: "+ studentScore1);
+        System.out.println("이름: " + studentNumber2 + " 나이: " + studentAge2 + " 성적: "+ studentScore2);
+
+    }
+}
---
+++ src/section1/classStart2.java
@@ -0,0 +1,17 @@
+package section1;
+
+public class classStart2 {
+    public static void main(String[] args) {
+        String[] studentNumber = {"학생1", "학생2", "학생3"};
+        int[] studentAge = {15,16,18};
+        int[] stusdentScore = {80,90,95};
+
+        for(int i=0;i<studentNumber.length; i++){
+            System.out.println("이름 : " + studentNumber[i] + " 나이: " + studentAge[i] + " 점수: " + stusdentScore[i]);
+
+
+        }
+
+
+    }
+}
