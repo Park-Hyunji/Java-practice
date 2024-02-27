f7045500704c75b484971cce114bbcbe11d7bee2
hyunji <phj9573@naver.com>
2024-02-27 23:56:49 +0900

    fix

--- src/section1/classStart2.java
+++ src/section1/classStart2.java
@@ -5,6 +5,7 @@ public class classStart2 {
         String[] studentNumber = {"학생1", "학생2", "학생3"};
         int[] studentAge = {15,16,18};
         int[] stusdentScore = {80,90,95};
+        // int
 
         for(int i=0;i<studentNumber.length; i++){
             System.out.println("이름 : " + studentNumber[i] + " 나이: " + studentAge[i] + " 점수: " + stusdentScore[i]);
